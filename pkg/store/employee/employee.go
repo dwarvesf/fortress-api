3cf969e0144f490728684d97b8c985ee5c22727d
Nam Nguyen <namnhce@gmail.com>
2022-11-15 11:02:03 +0700

    feat: preload enhancement

--- pkg/store/employee/employee.go
+++ pkg/store/employee/employee.go
@@ -28,8 +28,61 @@ func (s *store) OneByTeamEmail(teamEmail string) (*model.Employee, error) {
 	return employee, s.db.Where("team_email = ?", teamEmail).First(&employee).Error
 }
 
+type Preloadable interface {
+	Connection() string
+	PreloadParams() []interface{}
+}
+
+type PreloadString string
+
+func (p PreloadString) Connection() string {
+	return string(p)
+}
+
+func (p PreloadString) PreloadParams() []interface{} {
+	return []interface{}{p}
+}
+
+type PreloadParam struct {
+	Field     string
+	Condition string
+}
+
+func (p PreloadParam) Connection() string {
+	return p.Field
+}
+
+func (p PreloadParam) PreloadParams() []interface{} {
+	return []interface{}{p.Connection, p.Condition}
+}
+
+func InitPreloadable(params ...interface{}) Preloadable {
+
+	if len(params) == 0 {
+		panic("params is empty")
+	}
+
+	if len(params) == 1 {
+		return PreloadString(params[0].(string))
+	}
+
+	var rs []Preloadable
+	for _, p := range params {
+		switch p.(type) {
+		case string:
+			rs = append(rs, PreloadString(p.(string)))
+		}
+	}
+	return PreloadParam{
+		Field:     params[0].(string),
+		Condition: params[1].(string),
+	}
+}
+
+var preloadConfig = map[string]Preloadable{}
+
 // Search get employees by filter and pagination
-func (s *store) Search(filter SearchFilter, pagination model.Pagination) ([]*model.Employee, int64, error) {
+func (s *store) Search(filter SearchFilter, pagination model.Pagination, preloads ...Preloadable) ([]*model.Employee, int64, error) {
 	var total int64
 	var employees []*model.Employee
 
@@ -46,14 +99,31 @@ func (s *store) Search(filter SearchFilter, pagination model.Pagination) ([]*mod
 		query = query.Limit(limit)
 	}
 
-	query = query.Preload("ProjectMembers", "deleted_at IS NULL").
-		Preload("ProjectMembers.Project").
-		Preload("ProjectMembers.Project.Heads").
-		Preload("EmployeePositions", "deleted_at IS NULL").
-		Preload("EmployeePositions.Position").
-		Offset(offset)
+	query = PreloadData(query, []Preloadable{
+		InitPreloadable("ProjectMembers", "deleted_at IS NULL"),
+		InitPreloadable("ProjectMembers.Project"),
+		InitPreloadable("ProjectMembers.Project.Heads"),
+		InitPreloadable("EmployeePositions.Position"),
+		InitPreloadable("EmployeePositions", "deleted_at IS NULL"),
+	})
+
+	// query = query.Preload("ProjectMembers", "deleted_at IS NULL").
+	// 	Preload("ProjectMembers.Project").
+	// 	Preload("ProjectMembers.Project.Heads").
+	// 	Preload("EmployeePositions", "deleted_at IS NULL").
+	// 	Preload("EmployeePositions.Position").
+	// 	Offset(offset)
+
+	query = query.Offset(offset)
 
 	return employees, total, query.Find(&employees).Error
+}
+
+func PreloadData(db *gorm.DB, preloads []Preloadable) *gorm.DB {
+	for _, preload := range preloads {
+		db = db.Preload(preload.Connection(), preload.PreloadParams()...)
+	}
+	return db
 }
 
 func (s *store) UpdateEmployeeStatus(employeeID string, accountStatus model.AccountStatus) (*model.Employee, error) {
