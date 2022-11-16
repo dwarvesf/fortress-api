23b0523d9ccc1b52f5907555754ceb755191068c
Nam Nguyen <namnhce@gmail.com>
2022-11-16 14:11:00 +0700

    chore: update gorm v2 in base model

--- pkg/model/base.go
+++ pkg/model/base.go
@@ -3,28 +3,26 @@ package model
 import (
 	"time"
 
-	"github.com/jinzhu/gorm"
-	uuid "github.com/satori/go.uuid"
+	"gorm.io/gorm"
 )
 
 // BaseModel base model for domain type
 type BaseModel struct {
-	ID        UUID       `sql:",type:uuid" json:"id" gorm:"default:uuid()"`
-	CreatedAt time.Time  `sql:"default:now()" json:"createdAt"`
-	UpdatedAt *time.Time `sql:"default:now()" json:"updatedAt"`
-	DeletedAt *time.Time `json:"deletedAt,omitempty"`
+	ID        UUID            `sql:",type:uuid" json:"id" gorm:"default:uuid()"`
+	CreatedAt time.Time       `sql:"default:now()" json:"createdAt"`
+	UpdatedAt *time.Time      `sql:"default:now()" json:"updatedAt"`
+	DeletedAt *gorm.DeletedAt `json:"deletedAt,omitempty"`
 }
 
 // BeforeCreate prepare data before create data
-func (m *BaseModel) BeforeCreate(tx *gorm.DB) error {
-	// tx.Scopes().SetSetColumn("ID", uuid.NewV4())
-	tx.Model(m).Update("id", uuid.NewV4())
-	tx.Model(m).Update("created_at", time.Now())
-	return nil
+func (m *BaseModel) BeforeCreate(tx *gorm.DB) (err error) {
+	m.ID = NewUUID()
+	m.CreatedAt = time.Now()
+	return
 }
 
 // BeforeUpdate prepare data before create data
-func (m *BaseModel) BeforeUpdate(tx *gorm.DB) error {
-	tx.Model(m).Update("updated_at", time.Now())
-	return nil
+func (m *BaseModel) BeforeUpdate(tx *gorm.DB) (err error) {
+	tx.Statement.SetColumn("updated_at", time.Now())
+	return
 }
