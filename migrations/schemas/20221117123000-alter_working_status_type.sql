2d633439ce033128299a1d8944cf568412ba0251
nguyennh <nguyennh4522@gmail.com>
2022-11-17 15:11:33 +0700

    fix: migrate add value

--- migrations/schemas/20221117123000-alter_working_status_type.sql
+++ migrations/schemas/20221117123000-alter_working_status_type.sql
@@ -1,5 +1,29 @@
 
 -- +migrate Up
-ALTER TYPE working_status ADD VALUE IF NOT EXISTS  'on-boarding';
+ALTER TABLE employees ALTER COLUMN working_status TYPE TEXT;
+
+DROP TYPE IF EXISTS working_status;
+
+CREATE TYPE working_status AS ENUM (
+    'left', 
+    'probation', 
+    'full-time', 
+    'contractor', 
+    'on-boarding'
+);
+
+ALTER TABLE employees ALTER COLUMN working_status TYPE working_status USING(working_status::working_status);
 
 -- +migrate Down
+ALTER TABLE employees ALTER COLUMN working_status TYPE TEXT;
+
+DROP TYPE IF EXISTS working_status;
+
+CREATE TYPE working_status AS ENUM (
+    'left', 
+    'probation', 
+    'full-time', 
+    'contractor'
+);
+
+ALTER TABLE employees ALTER COLUMN working_status TYPE working_status USING(working_status::working_status);
