0652fbed1f9ed4c8ef4236613fc3f5fbc6c3421a
nguyennh4522 <nguyennh4522@gmail.com>
2022-12-21 11:15:49 +0700

    chore: add answer for work survey questions

--- pkg/model/questions.go
+++ pkg/model/questions.go
@@ -109,3 +109,132 @@ type QuestionDomainCount struct {
 	Domain QuestionDomain
 	LikertScaleCount
 }
+
+// AgreementLevel type for work survey answer
+type AgreementLevel string
+
+// AgreementLevel values
+const (
+	// engagement
+	AgreementLevelStronglyDisagree AgreementLevel = "strongly-disagree"
+	AgreementLevelDisagree         AgreementLevel = "disagree"
+	AgreementLevelMixed            AgreementLevel = "mixed"
+	AgreementLevelAgree            AgreementLevel = "agree"
+	AgreementLevelStronglyAgree    AgreementLevel = "strongly-agree"
+
+	// work - workload domain
+	AgreementLevelBreeze          AgreementLevel = "breeze"
+	AgreementLevelCouldHandleMore AgreementLevel = "could-handle-more"
+	AgreementLevelManageable      AgreementLevel = "manageable"
+	AgreementLevelOverwhelming    AgreementLevel = "overwhelming"
+	AgreementLevelCantKeepUp      AgreementLevel = "cant-keep-up"
+
+	// work - deadline domain
+	AgreementLevelVeryUncertain     AgreementLevel = "very-uncertain"
+	AgreementLevelSomewhatUncertain AgreementLevel = "somewhat-uncertain"
+	AgreementLevelNeutral           AgreementLevel = "neutral"
+	AgreementLevelSomewhatConfident AgreementLevel = "somewhat-confident"
+	AgreementLevelVeryConfident     AgreementLevel = "very-confident"
+
+	// work - learning domain
+	AgreementLevelVeryLittle AgreementLevel = "very-little"
+	AgreementLevelSomewhat   AgreementLevel = "somewhat"
+	AgreementLevelModerate   AgreementLevel = "moderate"
+	AgreementLevelALot       AgreementLevel = "a-lot"
+	AgreementLevelExtremely  AgreementLevel = "extremely"
+)
+
+// IsValid validation for AgreementLevel
+func (e AgreementLevel) IsValid() bool {
+	switch e {
+	case
+		AgreementLevelStronglyDisagree,
+		AgreementLevelDisagree,
+		AgreementLevelMixed,
+		AgreementLevelAgree,
+		AgreementLevelStronglyAgree,
+		AgreementLevelBreeze,
+		AgreementLevelCouldHandleMore,
+		AgreementLevelManageable,
+		AgreementLevelOverwhelming,
+		AgreementLevelCantKeepUp,
+		AgreementLevelVeryUncertain,
+		AgreementLevelSomewhatUncertain,
+		AgreementLevelNeutral,
+		AgreementLevelSomewhatConfident,
+		AgreementLevelVeryConfident,
+		AgreementLevelVeryLittle,
+		AgreementLevelSomewhat,
+		AgreementLevelModerate,
+		AgreementLevelALot,
+		AgreementLevelExtremely:
+		return true
+	}
+	return false
+}
+
+// String returns the string type of AgreementLevel
+func (e AgreementLevel) String() string {
+	return string(e)
+}
+
+var ArgreementLevelMap = map[AgreementLevel]string{
+	// engagement
+	AgreementLevelStronglyDisagree: "1",
+	AgreementLevelDisagree:         "2",
+	AgreementLevelMixed:            "3",
+	AgreementLevelAgree:            "4",
+	AgreementLevelStronglyAgree:    "5",
+
+	// workload
+	AgreementLevelBreeze:          "1",
+	AgreementLevelCouldHandleMore: "2",
+	AgreementLevelManageable:      "3",
+	AgreementLevelOverwhelming:    "4",
+	AgreementLevelCantKeepUp:      "5",
+
+	// deadline
+	AgreementLevelVeryUncertain:     "1",
+	AgreementLevelSomewhatUncertain: "2",
+	AgreementLevelNeutral:           "3",
+	AgreementLevelSomewhatConfident: "4",
+	AgreementLevelVeryConfident:     "5",
+
+	// learning
+	AgreementLevelVeryLittle: "1",
+	AgreementLevelSomewhat:   "2",
+	AgreementLevelModerate:   "3",
+	AgreementLevelALot:       "4",
+	AgreementLevelExtremely:  "5",
+}
+
+var ArgreementLevelValueMap = map[string]map[string]AgreementLevel{
+	EventSubtypeEngagement.String(): {
+		"1": AgreementLevelStronglyDisagree,
+		"2": AgreementLevelDisagree,
+		"3": AgreementLevelMixed,
+		"4": AgreementLevelAgree,
+		"5": AgreementLevelStronglyAgree,
+	},
+	QuestionDomainWorkload.String(): {
+		"1": AgreementLevelBreeze,
+		"2": AgreementLevelCouldHandleMore,
+		"3": AgreementLevelManageable,
+		"4": AgreementLevelOverwhelming,
+		"5": AgreementLevelCantKeepUp,
+	},
+	QuestionDomainDeadline.String(): {
+		"1": AgreementLevelVeryUncertain,
+		"2": AgreementLevelSomewhatUncertain,
+		"3": AgreementLevelNeutral,
+		"4": AgreementLevelSomewhatConfident,
+		"5": AgreementLevelVeryConfident,
+	},
+	QuestionDomainLearning.String(): {
+		"1": AgreementLevelVeryLittle,
+		"2": AgreementLevelSomewhat,
+		"3": AgreementLevelModerate,
+		"4": AgreementLevelALot,
+		"5": AgreementLevelExtremely,
+	},
+}
