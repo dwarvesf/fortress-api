f19e88f85b4278cb9a017f97146e624773861fd5
Hoang Nguyen <nnhuyhoang97@gmail.com>
2022-12-21 17:03:17 +0700

    feat: add trigger when create new employees

--- migrations/schemas/20221221053842-add_full_text_search_employees_table.sql
+++ migrations/schemas/20221221053842-add_full_text_search_employees_table.sql
@@ -13,8 +13,6 @@ ALTER TEXT SEARCH CONFIGURATION public.english_nostop
 
 CREATE INDEX idx_employees_keyword_vector ON "employees" USING gin(keyword_vector);
 
-SELECT full_name || discord_id FROM employees;
-
 UPDATE
       "employees" c
     SET
@@ -25,7 +23,37 @@ UPDATE
             unnest(to_tsvector(LOWER(c."full_name" || ' ' || c."team_email" || ' ' || c."discord_id" || ' ' || c."notion_id" || ' ' || c."github_id" ))),
             generate_series(1, length(lexeme)) len));
 
+-- +migrate StatementBegin
+CREATE OR REPLACE FUNCTION fn_insert_keyword_vector ()
+	RETURNS TRIGGER
+	AS $BODY$
+BEGIN
+  UPDATE
+        "employees" c
+      SET
+        "keyword_vector" = array_to_tsvector ((
+            SELECT
+              array_agg(DISTINCT substring(lexeme FOR len))
+            FROM
+              unnest(to_tsvector(LOWER(c."full_name" || ' ' || c."team_email" || ' ' || c."discord_id" || ' ' || c."notion_id" || ' ' || c."github_id" ))),
+              generate_series(1, length(lexeme)) len));
+      RETURN new;
+END;
+$BODY$
+LANGUAGE plpgsql;
+-- +migrate StatementEnd
+
+CREATE TRIGGER trig_insert_keyword_vector
+     AFTER INSERT ON employees
+     FOR EACH ROW
+     EXECUTE PROCEDURE fn_insert_keyword_vector();
+
+
 -- +migrate Down
+
+DROP TRIGGER IF EXISTS trig_insert_keyword_vector ON employees;
+DROP FUNCTION fn_insert_keyword_vector;
+
 ALTER TABLE "employees" DROP COLUMN "keyword_vector";
 DROP TEXT SEARCH CONFIGURATION  IF EXISTS public.english_nostop;
 DROP TEXT SEARCH DICTIONARY  IF EXISTS  english_stem_nostop;
