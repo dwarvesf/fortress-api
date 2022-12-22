1c57445bc81494f26daa4d263bd0062cbe1fb4d4
Nam Nguyen <namnhce@gmail.com>
2022-12-22 16:43:20 +0700

    chore: update migration

--- migrations/schemas/20221212015000-alter_event_reviewer_table.sql
+++ migrations/schemas/20221212015000-alter_event_reviewer_table.sql
@@ -6,8 +6,6 @@ ALTER TABLE "employee_event_reviewers" ADD COLUMN "is_forced_done" BOOLEAN DEFAU
 ALTER TABLE "employee_event_questions" ADD COLUMN "domain" TEXT;
 ALTER TABLE "questions" ADD COLUMN "domain" TEXT;
 ALTER TABLE "projects" ADD COLUMN "allows_sending_survey" BOOLEAN DEFAULT FALSE;
-ALTER TABLE "employees" ADD COLUMN "discord_name" TEXT;
-ALTER TABLE "employees" ADD COLUMN "notion_name" TEXT;
 
 -- +migrate Down
 ALTER TABLE "employee_event_reviewers" ADD COLUMN "status" TEXT;
@@ -17,5 +15,3 @@ ALTER TABLE "employee_event_reviewers" DROP COLUMN "is_forced_done";
 ALTER TABLE "employee_event_questions" DROP COLUMN "domain";
 ALTER TABLE "questions" DROP COLUMN "domain";
 ALTER TABLE "projects" DROP COLUMN "allows_sending_survey";
-ALTER TABLE "employees" DROP COLUMN "discord_name";
-ALTER TABLE "employees" DROP COLUMN "notion_name";
