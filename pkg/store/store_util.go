f11e17b1db4b2f9220df504670f915a079b26775
nguyennh4522 <nguyennh4522@gmail.com>
2022-12-29 16:17:53 +0700

    chore: add debug mode

--- pkg/store/store_util.go
+++ pkg/store/store_util.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/dwarvesf/fortress-api/pkg/config"
 	"github.com/dwarvesf/fortress-api/pkg/logger"
+	gormlogger "gorm.io/gorm/logger"
 )
 
 // NewPostgresStore postgres init by gorm
@@ -38,33 +39,9 @@ func NewPostgresStore(cfg *config.Config) DBRepo {
 
 	logger.L.Info("database connected")
 
+	if cfg.Debug {
+		db.Logger = gormlogger.Default.LogMode(gormlogger.Info)
+	}
+
 	return &repo{Database: db}
-}
-
-func connDb(cfg *config.Config) *gorm.DB {
-	ds := fmt.Sprintf(
-		"postgres://%s:%s@%s:%s/%s?sslmode=disable",
-		cfg.Postgres.User, cfg.Postgres.Pass,
-		cfg.Postgres.Host, cfg.Postgres.Port, cfg.Postgres.Name,
-	)
-
-	conn, err := sql.Open("postgres", ds)
-	if err != nil {
-		logger.L.Fatalf(err, "failed to open database connection")
-	}
-
-	db, err := gorm.Open(postgres.New(
-		postgres.Config{Conn: conn}),
-		&gorm.Config{
-			NamingStrategy: schema.NamingStrategy{
-				SingularTable: false,
-			},
-		})
-	if err != nil {
-		logger.L.Fatalf(err, "failed to open database connection")
-	}
-
-	logger.L.Info("database connected")
-
-	return db
 }
