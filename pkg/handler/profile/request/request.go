7899e33a9ad390c7b5bf5580e7c9be6a10c7e173
Nam Nguyen <namnhce@gmail.com>
2022-12-29 23:30:24 +0700

    chore: ignore updating social data in profile update

--- pkg/handler/profile/request/request.go
+++ pkg/handler/profile/request/request.go
@@ -7,13 +7,6 @@ type UpdateInfoInput struct {
 	TeamEmail        string `form:"teamEmail" json:"teamEmail" binding:"required,email"`
 	PersonalEmail    string `form:"personalEmail" json:"personalEmail" binding:"required,email"`
 	PhoneNumber      string `form:"phoneNumber" json:"phoneNumber" binding:"required,max=12,min=10"`
-	GithubID         string `form:"githubID" json:"githubID"`
-	NotionID         string `form:"notionID" json:"notionID"`
-	NotionName       string `form:"notionName" json:"notionName"`
-	NotionEmail      string `form:"notionEmail" json:"notionEmail"`
-	DiscordID        string `form:"discordID" json:"discordID"`
-	DiscordName      string `form:"discordName" json:"discordName"`
-	LinkedInName     string `form:"linkedInName" json:"linkedInName"`
 	PlaceOfResidence string `form:"placeOfResidence" json:"placeOfResidence" binding:"required"`
 	Address          string `form:"address" json:"address"`
 	Country          string `form:"country" json:"country"`
@@ -28,14 +21,4 @@ func (i UpdateInfoInput) MapEmployeeInput(employee *model.Employee) {
 	employee.Address = i.Address
 	employee.City = i.City
 	employee.Country = i.Country
-
-	employee.DiscordID = i.DiscordID
-	employee.DiscordName = i.DiscordName
-
-	employee.NotionID = i.NotionID
-	employee.NotionName = i.NotionName
-	employee.NotionEmail = i.NotionEmail
-
-	employee.GithubID = i.GithubID
-	employee.LinkedInName = i.LinkedInName
 }
