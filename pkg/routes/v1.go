5f4b7493fe950f8be0b73923023959a23b311d05
Nam Nguyen <namnhce@gmail.com>
2022-12-30 15:32:52 +0700

    chore: change project status permission to update

--- pkg/routes/v1.go
+++ pkg/routes/v1.go
@@ -52,7 +52,7 @@ func loadV1Routes(r *gin.Engine, h *handler.Handler, repo store.DBRepo, s *store
 	v1.GET("/projects/:id", amw.WithAuth, pmw.WithPerm("projects.read"), h.Project.Details)
 	v1.PUT("/projects/:id/sending-survey-state", amw.WithAuth, pmw.WithPerm("projects.edit"), h.Project.UpdateSendingSurveyState)
 	v1.POST("/projects/:id/upload-avatar", amw.WithAuth, pmw.WithPerm("projects.edit"), h.Project.UploadAvatar)
-	v1.PUT("/projects/:id/status", amw.WithAuth, pmw.WithPerm("projects.read"), h.Project.UpdateProjectStatus)
+	v1.PUT("/projects/:id/status", amw.WithAuth, pmw.WithPerm("projects.edit"), h.Project.UpdateProjectStatus)
 	v1.POST("/projects/:id/members", amw.WithAuth, pmw.WithPerm("projectMembers.create"), h.Project.AssignMember)
 	v1.GET("/projects/:id/members", amw.WithAuth, pmw.WithPerm("projectMembers.read"), h.Project.GetMembers)
 	v1.PUT("/projects/:id/members", amw.WithAuth, pmw.WithPerm("projectMembers.edit"), h.Project.UpdateMember)
