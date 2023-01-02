c1fb551a9444c9bec34d5b7cfe860403a251b15b
nguyennh4522 <nguyennh4522@gmail.com>
2023-01-03 00:17:32 +0700

    Revert "feat: get dashboard resource utilization"
    This reverts commit 7679921a7c3fa6bb8e1899ee7d25b37f5cf88202.

--- pkg/store/dashboard/interface.go
+++ pkg/store/dashboard/interface.go
@@ -1,10 +1,3 @@
 package dashboard
 
-import (
-	"github.com/dwarvesf/fortress-api/pkg/model"
-	"gorm.io/gorm"
-)
-
-type IStore interface {
-	GetResourceUtilizationByYear(db *gorm.DB, year int) ([]*model.ResourceUtilization, error)
-}
+type IStore interface{}
