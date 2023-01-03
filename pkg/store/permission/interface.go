2154653786f0f2bf3b441a38263c8f9572bb6e28
nguyennh4522 <nguyennh4522@gmail.com>
2023-01-03 15:46:48 +0700

    chore: add filterByStatus permission

--- pkg/store/permission/interface.go
+++ pkg/store/permission/interface.go
@@ -8,4 +8,5 @@ import (
 
 type IStore interface {
 	GetByEmployeeID(db *gorm.DB, employeeID string) (permissions []*model.Permission, err error)
+	HasPermission(db *gorm.DB, employeeID string, perm string) (bool, error)
 }
