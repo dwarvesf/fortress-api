e196a04f38b4970f7753a8d2876b3fc6bd283b14
datphamcode295 <vandatpro2000@gmail.com>
2023-01-06 14:43:39 +0700

    feat: dashboard project size

--- pkg/store/dashboard/dashboard.go
+++ pkg/store/dashboard/dashboard.go
@@ -1,7 +1,25 @@
 package dashboard
 
+import (
+	"github.com/dwarvesf/fortress-api/pkg/model"
+	"gorm.io/gorm"
+)
+
 type store struct{}
 
 func New() IStore {
 	return &store{}
+}
+
+func (s *store) GetProjectSizes(db *gorm.DB) ([]*model.ProjectSize, error) {
+	var ru []*model.ProjectSize
+
+	query := `
+		SELECT projects.id, projects.name, projects.code, count(*) AS size
+			FROM (projects join project_members pm ON projects.id = pm.project_id)
+			WHERE projects.function = 'development' AND pm.status = 'active' 
+			GROUP BY projects.id
+	`
+
+	return ru, db.Debug().Raw(query).Scan(&ru).Error
 }
