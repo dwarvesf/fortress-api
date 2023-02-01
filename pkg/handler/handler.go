9816fa59f06b9f36cd864215e0e5215b1ec9a9d2
lmquang <quanglm.ops@gmail.com>
2023-02-01 13:35:50 +0700

    feat: add staffing demand api

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -14,6 +14,7 @@ import (
 	"github.com/dwarvesf/fortress-api/pkg/handler/metadata"
 	"github.com/dwarvesf/fortress-api/pkg/handler/profile"
 	"github.com/dwarvesf/fortress-api/pkg/handler/project"
+	"github.com/dwarvesf/fortress-api/pkg/handler/staffingdemand"
 	"github.com/dwarvesf/fortress-api/pkg/handler/survey"
 	"github.com/dwarvesf/fortress-api/pkg/handler/techradar"
 	"github.com/dwarvesf/fortress-api/pkg/handler/valuation"
@@ -23,39 +24,41 @@ import (
 )
 
 type Handler struct {
-	Healthcheck healthz.IHandler
-	Employee    employee.IHandler
-	Metadata    metadata.IHandler
-	Auth        auth.IHandler
-	Project     project.IHandler
-	Profile     profile.IHandler
-	Feedback    feedback.IHandler
-	Survey      survey.IHandler
-	Dashboard   dashboard.IHandler
-	Valuation   valuation.IHandler
-	Earn        earn.IHandler
-	TechRadar   techradar.IHandler
-	Audience    audience.IHandler
-	Event       event.IHandler
-	Hiring      hiring.IHandler
+	Healthcheck    healthz.IHandler
+	Employee       employee.IHandler
+	Metadata       metadata.IHandler
+	Auth           auth.IHandler
+	Project        project.IHandler
+	Profile        profile.IHandler
+	Feedback       feedback.IHandler
+	Survey         survey.IHandler
+	Dashboard      dashboard.IHandler
+	Valuation      valuation.IHandler
+	Earn           earn.IHandler
+	TechRadar      techradar.IHandler
+	Audience       audience.IHandler
+	Event          event.IHandler
+	Hiring         hiring.IHandler
+	StaffingDemand staffingdemand.IHandler
 }
 
 func New(store *store.Store, repo store.DBRepo, service *service.Service, logger logger.Logger, cfg *config.Config) *Handler {
 	return &Handler{
-		Healthcheck: healthz.New(),
-		Employee:    employee.New(store, repo, service, logger, cfg),
-		Metadata:    metadata.New(store, repo, service, logger, cfg),
-		Auth:        auth.New(store, repo, service, logger, cfg),
-		Project:     project.New(store, repo, service, logger, cfg),
-		Profile:     profile.New(store, repo, service, logger, cfg),
-		Feedback:    feedback.New(store, repo, service, logger, cfg),
-		Survey:      survey.New(store, repo, service, logger, cfg),
-		Dashboard:   dashboard.New(store, repo, service, logger, cfg),
-		Valuation:   valuation.New(store, repo, service, logger, cfg),
-		Earn:        earn.New(store, repo, service, logger, cfg),
-		TechRadar:   techradar.New(store, repo, service, logger, cfg),
-		Audience:    audience.New(store, repo, service, logger, cfg),
-		Event:       event.New(store, repo, service, logger, cfg),
-		Hiring:      hiring.New(store, repo, service, logger, cfg),
+		Healthcheck:    healthz.New(),
+		Employee:       employee.New(store, repo, service, logger, cfg),
+		Metadata:       metadata.New(store, repo, service, logger, cfg),
+		Auth:           auth.New(store, repo, service, logger, cfg),
+		Project:        project.New(store, repo, service, logger, cfg),
+		Profile:        profile.New(store, repo, service, logger, cfg),
+		Feedback:       feedback.New(store, repo, service, logger, cfg),
+		Survey:         survey.New(store, repo, service, logger, cfg),
+		Dashboard:      dashboard.New(store, repo, service, logger, cfg),
+		Valuation:      valuation.New(store, repo, service, logger, cfg),
+		Earn:           earn.New(store, repo, service, logger, cfg),
+		TechRadar:      techradar.New(store, repo, service, logger, cfg),
+		Audience:       audience.New(store, repo, service, logger, cfg),
+		Event:          event.New(store, repo, service, logger, cfg),
+		Hiring:         hiring.New(store, repo, service, logger, cfg),
+		StaffingDemand: staffingdemand.New(store, repo, service, logger, cfg),
 	}
 }
---
+++ pkg/handler/staffingdemand/staffing_demand.go
@@ -0,0 +1,74 @@
+// please edit this file only with approval from hnh
+package staffingdemand
+
+import (
+	"net/http"
+
+	"github.com/dstotijn/go-notion"
+	"github.com/dwarvesf/fortress-api/pkg/config"
+	"github.com/dwarvesf/fortress-api/pkg/logger"
+	"github.com/dwarvesf/fortress-api/pkg/model"
+	"github.com/dwarvesf/fortress-api/pkg/service"
+	"github.com/dwarvesf/fortress-api/pkg/store"
+	"github.com/dwarvesf/fortress-api/pkg/view"
+	"github.com/gin-gonic/gin"
+)
+
+type handler struct {
+	store   *store.Store
+	service *service.Service
+	logger  logger.Logger
+	repo    store.DBRepo
+	config  *config.Config
+}
+
+// New returns a handler
+func New(store *store.Store, repo store.DBRepo, service *service.Service, logger logger.Logger, cfg *config.Config) IHandler {
+	return &handler{
+		store:   store,
+		repo:    repo,
+		service: service,
+		logger:  logger,
+		config:  cfg,
+	}
+}
+
+// List godoc
+// @Summary Get list  staffing demands from DF Staffing Demand
+// @Description Get list  staffing demands from DF Staffing Demand
+// @Tags staffing-demands
+// @Accept  json
+// @Produce  json
+// @Success 200 {object} []model.StaffingDemand
+// @Failure 400 {object} view.ErrorResponse
+func (h *handler) List(c *gin.Context) {
+	resp, err := h.service.Notion.GetDatabase(h.config.Notion.StaffingDemandDBID, nil, nil)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, view.CreateResponse[any](nil, nil, err, nil, "can't get staffing demands from notion"))
+		return
+	}
+
+	var staffingDemands = []model.StaffingDemand{}
+
+	for _, r := range resp.Results {
+		props := r.Properties.(notion.DatabasePageProperties)
+
+		name := props["Project Name"].Title[0].Text.Content
+		if r.Icon != nil && r.Icon.Emoji != nil {
+			name = *r.Icon.Emoji + " " + props["Name"].Title[0].Text.Content
+		}
+
+		request := ""
+		if len(props["Request"].RichText) > 0 {
+			request = props["Request"].RichText[0].Text.Content
+		}
+
+		staffingDemands = append(staffingDemands, model.StaffingDemand{
+			ID:      r.ID,
+			Name:    name,
+			Request: request,
+		})
+	}
+
+	c.JSON(http.StatusOK, view.CreateResponse[any](staffingDemands, nil, nil, nil, "get list staffing demands successfully"))
+}
