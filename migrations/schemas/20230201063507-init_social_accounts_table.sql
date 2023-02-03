8a94cdc9e9d3d3407a25ae5ae2546d9aa7e9ca7f
Nam Nguyen <namnhce@gmail.com>
2023-02-04 02:36:26 +0700

    fix: remove empty social account record in migration

--- migrations/schemas/20230201063507-init_social_accounts_table.sql
+++ migrations/schemas/20230201063507-init_social_accounts_table.sql
@@ -15,26 +15,6 @@ CREATE TABLE IF NOT EXISTS social_accounts (
 ALTER TABLE social_accounts
     ADD CONSTRAINT social_accounts_employee_id_fkey FOREIGN KEY (employee_id) REFERENCES employees (id);
 
-INSERT INTO social_accounts (deleted_at, created_at, updated_at, employee_id, "type", account_id, email, display_name)
-SELECT NULL, NOW(), NOW(), e.id, 'github', e.github_id, NULL, e.github_id
-FROM employees e;
-
-INSERT INTO social_accounts (deleted_at, created_at, updated_at, employee_id, "type", account_id, email, display_name)
-SELECT NULL, NOW(), NOW(), e.id, 'gitlab', e.gitlab_id, NULL, e.gitlab_id 
-FROM employees e;
-
-INSERT INTO social_accounts (deleted_at, created_at, updated_at, employee_id, "type", account_id, email, display_name)
-SELECT NULL, NOW(), NOW(), e.id, 'notion', e.notion_id, e.notion_email, e.notion_name 
-FROM employees e;
-
-INSERT INTO social_accounts (deleted_at, created_at, updated_at, employee_id, "type", account_id, email, display_name)
-SELECT NULL, NOW(), NOW(), e.id, 'discord', e.discord_id, NULL, e.discord_name
-FROM employees e;
-
-INSERT INTO social_accounts (deleted_at, created_at, updated_at, employee_id, "type", account_id, email, display_name)
-SELECT NULL, NOW(), NOW(), e.id, 'linkedin', e.linkedin_name, NULL, e.linkedin_name
-FROM employees e;
-
 -- +migrate Down
 ALTER TABLE social_accounts DROP CONSTRAINT IF EXISTS social_accounts_employee_id_fkey;
 
