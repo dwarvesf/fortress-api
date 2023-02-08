5fa3a1abf8d3281a8f2e8a3dbc0d394d91de4529
nguyennh4522 <nguyennh4522@gmail.com>
2023-02-09 02:47:46 +0700

    feat: refactor project member

--- pkg/store/projecthead/project_head.go
+++ pkg/store/projecthead/project_head.go
@@ -39,15 +39,27 @@ func (s *store) UpdateSelectedFieldsByID(db *gorm.DB, id string, updateModel mod
 }
 
 // One Get one head by project id and position
-func (s *store) One(db *gorm.DB, projectID string, position model.HeadPosition) (*model.ProjectHead, error) {
+func (s *store) One(db *gorm.DB, projectID string, employeeID string, position model.HeadPosition) (*model.ProjectHead, error) {
 	var projectHead *model.ProjectHead
-	return projectHead, db.Where("project_id = ? AND position = ? AND end_date IS NULL", projectID, position).First(&projectHead).Error
+	return projectHead, db.
+		Where("project_id = ?", projectID).
+		Where("employee_id = ?", employeeID).
+		Where("position = ?", position).
+		First(&projectHead).Error
 }
 
-func (s *store) UpdateEndDateOfEmployee(db *gorm.DB, employeeID string, projectID string, position string, endDate time.Time) (*model.ProjectHead, error) {
+func (s *store) UpdateDateOfEmployee(db *gorm.DB, employeeID string, projectID string, position string, startDate *time.Time, endDate *time.Time) (*model.ProjectHead, error) {
 	head := model.ProjectHead{}
 	return &head, db.
 		Model(&head).
-		Where("employee_id = ? AND project_id = ? AND position = ? AND end_date IS NULL", employeeID, projectID, position).
-		Update("end_date", endDate).Error
+		Where("employee_id = ? AND project_id = ? AND position = ?", employeeID, projectID, position).
+		Updates(map[string]interface{}{
+			"start_date": startDate,
+			"end_date":   endDate,
+		}).Error
+}
+
+func (s *store) GetByProjectIDAndPosition(db *gorm.DB, projectID string, position model.HeadPosition) ([]*model.ProjectHead, error) {
+	var heads []*model.ProjectHead
+	return heads, db.Where("project_id = ? AND position = ?", projectID, position).Find(&heads).Error
 }
