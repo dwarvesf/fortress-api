781322ecbaa674151034a40f4cce40d79b731e1d
Nam Nguyen <namnhce@gmail.com>
2023-03-02 18:56:17 +0700

    chore: add more validate cond for send invoice request

--- pkg/handler/invoice/request/request.go
+++ pkg/handler/invoice/request/request.go
@@ -2,46 +2,15 @@ package request
 
 import (
 	"encoding/json"
-	"regexp"
+	"strings"
 
+	"github.com/gin-gonic/gin"
+
+	"github.com/dwarvesf/fortress-api/pkg/config"
 	"github.com/dwarvesf/fortress-api/pkg/handler/invoice/errs"
 	"github.com/dwarvesf/fortress-api/pkg/model"
+	"github.com/dwarvesf/fortress-api/pkg/utils/mailutils"
 )
-
-type CreateInput struct {
-	Number           string              `json:"number"`
-	DueAt            string              `json:"dueAt"`
-	PaidAt           string              `json:"paidAt"`
-	Status           model.InvoiceStatus `json:"status"`
-	Email            string              `json:"email"`
-	Description      string              `json:"description"`
-	Note             string              `json:"note"`
-	SubTotal         int64               `json:"subTotal"`
-	Tax              int64               `json:"tax"`
-	Discount         int64               `json:"discount"`
-	Total            int64               `json:"total"`
-	ConversionAmount int64               `json:"conversionAmount"`
-	Month            int                 `json:"month"`
-	Year             int                 `json:"year"`
-	ThreadID         string              `json:"threadID"`
-	ScheduledDate    string              `json:"scheduledDate"`
-	ConversionRate   float64             `json:"conversionRate"`
-	BankID           model.UUID          `json:"bankID" binding:"required"`
-	ProjectID        model.UUID          `json:"projectID"`
-}
-
-func (r *CreateInput) Validate() error {
-	if r.Status != "" && !r.Status.IsValid() {
-		return errs.ErrInvalidInvoiceStatus
-	}
-
-	regex, _ := regexp.Compile(".+@.+\\..+")
-	if r.Email != "" && !regex.MatchString(r.Email) {
-		return errs.ErrInvalidEmailDomain
-	}
-
-	return nil
-}
 
 type UpdateStatusRequest struct {
 	Status            model.InvoiceStatus `json:"status"`
@@ -104,6 +73,38 @@ func toInvoiceItemsModel(lineItems []InvoiceItem) []model.InvoiceItem {
 	}
 
 	return items
+}
+
+func (i *SendInvoiceRequest) ValidateAndMappingRequest(c *gin.Context, cfg *config.Config) error {
+	if err := c.ShouldBindJSON(&i); err != nil {
+		return err
+	}
+
+	var ccList []string
+	for _, cc := range i.CC {
+		if strings.TrimSpace(cc) == "" {
+			continue
+		}
+		ccList = append(ccList, cc)
+	}
+
+	i.CC = ccList
+
+	if cfg.Env == "prod" {
+		return nil
+	}
+
+	if !mailutils.IsDwarvesMail(i.Email) {
+		return errs.ErrInvalidDeveloperEmail
+	}
+
+	for _, v := range i.CC {
+		if !mailutils.IsDwarvesMail(v) {
+			return errs.ErrInvalidDeveloperEmail
+		}
+	}
+
+	return nil
 }
 
 func (i *SendInvoiceRequest) ToInvoiceModel() (*model.Invoice, error) {
