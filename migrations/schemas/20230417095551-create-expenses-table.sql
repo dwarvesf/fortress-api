96f6c10eb90f87874b312e35725d476f6850c3b0
lmquang <quanglm.ops@gmail.com>
2023-04-17 11:45:11 +0700

    chore: update migration file format

--- migrations/schemas/20230417095551-create-expenses-table.sql
+++ migrations/schemas/20230417095551-create-expenses-table.sql
@@ -1,28 +1,30 @@
-
 -- +migrate Up
 CREATE TABLE IF NOT EXISTS "expenses" (
-  "id" uuid PRIMARY KEY DEFAULT (uuid()),
-  "employee_id" uuid,
-  "reason" text COLLATE "pg_catalog"."default",
-  "issued_date" timestamptz(6) DEFAULT now(),
-  "amount" int8,
-  "created_at" timestamptz(6) DEFAULT now(),
-  "updated_at" timestamptz(6) DEFAULT now(),
-  "deleted_at" timestamptz(6),
-  "currency_id" uuid,
-  "currency" text COLLATE "pg_catalog"."default" DEFAULT 'VND'::text,
-  "invoice_image_url" varchar(255) COLLATE "pg_catalog"."default",
-  "metadata" json,
-  "accounting_transaction_id" uuid,
-  "basecamp_id" int8
+    "id"                        UUID PRIMARY KEY                    DEFAULT (uuid()),
+    "created_at"                TIMESTAMPTZ(6)                      DEFAULT NOW(),
+    "updated_at"                TIMESTAMPTZ(6)                      DEFAULT NOW(),
+    "deleted_at"                TIMESTAMPTZ(6),
+    "employee_id"               UUID,
+    "reason"                    TEXT COLLATE "pg_catalog"."default",
+    "issued_date"               TIMESTAMPTZ(6)                      DEFAULT NOW(),
+    "amount"                    INT8,
+    "currency_id"               UUID,
+    "currency"                  TEXT COLLATE "pg_catalog"."default" DEFAULT 'VND'::TEXT,
+    "invoice_image_url"         TEXT COLLATE "pg_catalog"."default",
+    "metadata"                  JSON,
+    "accounting_transaction_id" UUID,
+    "basecamp_id"               INT8
 )
 ;
-
 -- ----------------------------
 -- Foreign Keys structure for table expense
 -- ----------------------------
-ALTER TABLE "expenses" ADD CONSTRAINT "expenses_accounting_transaction_id_fkey" FOREIGN KEY ("accounting_transaction_id") REFERENCES "accounting_transactions" ("id") ON DELETE CASCADE ON UPDATE NO ACTION;
-ALTER TABLE "expenses" ADD CONSTRAINT "expenses_currency_id_fkey" FOREIGN KEY ("currency_id") REFERENCES "currencies" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
-
+ALTER TABLE "expenses"
+    ADD CONSTRAINT "expenses_accounting_transaction_id_fkey" FOREIGN KEY ("accounting_transaction_id") REFERENCES "accounting_transactions" ("id") ON DELETE CASCADE ON UPDATE NO ACTION;
+ALTER TABLE "expenses"
+    ADD CONSTRAINT "expenses_currency_id_fkey" FOREIGN KEY ("currency_id") REFERENCES "currencies" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 -- +migrate Down
+ALTER TABLE "expenses" DROP CONSTRAINT "expenses_accounting_transaction_id_fkey";
+ALTER TABLE "expenses" DROP CONSTRAINT "expenses_currency_id_fkey";
+DROP TABLE IF EXISTS "expenses";
