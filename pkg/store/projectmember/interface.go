2e1a6ffaffb8fa9596b9373ff5958ce354431c68
Nam Nguyen <namnhce@gmail.com>
2023-05-09 11:45:31 +0700

    feat: sync project member status

--- pkg/store/projectmember/interface.go
+++ pkg/store/projectmember/interface.go
@@ -6,17 +6,18 @@ import (
 )
 
 type IStore interface {
-	OneByID(db *gorm.DB, id string) (*model.ProjectMember, error)
-	OneBySlotID(db *gorm.DB, slotID string) (*model.ProjectMember, error)
 	Create(db *gorm.DB, member *model.ProjectMember) error
 	Delete(db *gorm.DB, id string) (err error)
-	IsExist(db *gorm.DB, id string) (bool, error)
-	IsExistsByEmployeeID(db *gorm.DB, projectID string, employeeID string) (bool, error)
-	UpdateSelectedFieldsByID(db *gorm.DB, id string, updateModel model.ProjectMember, updatedFields ...string) (*model.ProjectMember, error)
-	UpdateSelectedFieldByProjectID(db *gorm.DB, projectID string, updateModel model.ProjectMember, updatedField string) error
-	UpdateEndDateByProjectID(db *gorm.DB, projectID string) error
 	GetActiveByProjectIDs(db *gorm.DB, projectIDs []string) ([]*model.ProjectMember, error)
 	GetActiveMemberInProject(db *gorm.DB, projectID string, employeeID string) (*model.ProjectMember, error)
 	GetActiveMembersBySlotID(db *gorm.DB, slotID string) ([]*model.ProjectMember, error)
 	GetAssignedMembers(db *gorm.DB, projectID string, status string, preload bool) ([]*model.ProjectMember, error)
+	IsExist(db *gorm.DB, id string) (bool, error)
+	IsExistsByEmployeeID(db *gorm.DB, projectID string, employeeID string) (bool, error)
+	OneByID(db *gorm.DB, id string) (*model.ProjectMember, error)
+	OneBySlotID(db *gorm.DB, slotID string) (*model.ProjectMember, error)
+	UpdateEndDateByProjectID(db *gorm.DB, projectID string) error
+	UpdateExpMemberToInActive(db *gorm.DB) error
+	UpdateSelectedFieldByProjectID(db *gorm.DB, projectID string, updateModel model.ProjectMember, updatedField string) error
+	UpdateSelectedFieldsByID(db *gorm.DB, id string, updateModel model.ProjectMember, updatedFields ...string) (*model.ProjectMember, error)
 }
