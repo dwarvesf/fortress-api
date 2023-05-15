e930698dc49bff84a223e93d069253f3b28fa29a
trkhoi <ngotrongkhoi112@gmail.com>
2023-05-15 16:25:16 +0700

    chore: update logic icy tx
    chore: update logic icy tx
    
    chore: update logic icy tx

--- pkg/store/socialaccount/interface.go
+++ pkg/store/socialaccount/interface.go
@@ -12,4 +12,5 @@ type IStore interface {
 	GetByEmployeeID(db *gorm.DB, employeeID string) (accounts []*model.SocialAccount, err error)
 	GetByType(db *gorm.DB, saType string) (accounts []*model.SocialAccount, err error)
 	UpdateSelectedFieldsByID(db *gorm.DB, id string, updateModel model.SocialAccount, updatedFields ...string) (*model.SocialAccount, error)
+	GetByDiscordID(db *gorm.DB, discordID string) (*model.SocialAccount, error)
 }
