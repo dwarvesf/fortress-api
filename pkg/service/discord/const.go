20072f9a06bf347e69500b93a8493093c9eaff9b
namnhce <namnhce@gmail.com>
2023-07-21 15:32:32 +0700

    chore: enhance discord message for delivery metric

--- pkg/service/discord/const.go
+++ pkg/service/discord/const.go
@@ -7,6 +7,10 @@ var mapEmoji = map[string]string{
 	"BADGE2":              "<a:badge2:1131850991663337554>",
 	"BADGE3":              "<a:badge3:1131850996159610930>",
 	"BADGE5":              "<a:badge5:1131851001117294672>",
+	"LOG_CHANNEL":         "<:log_channel:1131863319377100841>",
+	"STAR_ANIMATED":       "<a:star_animated:1131862886592024586>",
+	"INCREASING_ANIMATED": "<a:increasing_animated:1131862879319097394>",
+	"CLOCK_NEW":           "<:clock_new:1131863089185292428>",
 }
 
 func getEmoji(emoji string) string {
