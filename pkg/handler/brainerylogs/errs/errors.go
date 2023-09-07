e7c3cf1cdc575fe3bf32557bdcbe884d072aab64
namnhce <namnhce@gmail.com>
2023-09-07 16:40:13 +0700

    feat: allow get brainery logs by date

--- pkg/handler/brainerylogs/errs/errors.go
+++ pkg/handler/brainerylogs/errs/errors.go
@@ -2,4 +2,7 @@ package errs
 
 import "errors"
 
-var ErrInvalidPublishedAt = errors.New("cannot parse publishedAt")
+var (
+	ErrInvalidPublishedAt = errors.New("cannot parse publishedAt")
+	ErrInvalidDateFormat  = errors.New("invalid date format")
+)
