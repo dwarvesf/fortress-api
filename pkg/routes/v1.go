c3bb66f5cc9b3f3a7d4c383038947820c9c1a380
bean <vhbien000@gmail.com>
2024-04-02 14:51:19 +0700

    fix: remove publish icy activity log (#690)
    Co-authored-by: Bean Vo <bean@d.foundation>

--- pkg/routes/v1.go
+++ pkg/routes/v1.go
@@ -31,7 +31,6 @@ func loadV1Routes(r *gin.Engine, h *handler.Handler, repo store.DBRepo, s *store
 		cronjob.POST("/delivery-metric-reports", amw.WithAuth, pmw.WithPerm(model.PermissionCronjobExecute), h.Discord.DeliveryMetricsReport)
 		cronjob.POST("/sync-delivery-metrics", amw.WithAuth, pmw.WithPerm(model.PermissionCronjobExecute), h.DeliveryMetric.Sync)
 		cronjob.POST("/sync-conversion-rates", amw.WithAuth, pmw.WithPerm(model.PermissionCronjobExecute), h.ConversionRate.Sync)
-		cronjob.POST("/publish-icy-activity-log", amw.WithAuth, pmw.WithPerm(model.PermissionCronjobExecute), h.Discord.PublishIcyActivityLog)
 	}
 
 	/////////////////
