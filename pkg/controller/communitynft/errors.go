a44196fa9114ac21df976f7ec7f7dfbfcb6ea737
phucledien <phucledien@gmail.com>
2024-04-23 08:54:57 +0700

    chore: update error

--- pkg/controller/communitynft/errors.go
+++ pkg/controller/communitynft/errors.go
@@ -3,6 +3,7 @@ package communitynft
 import "errors"
 
 var (
-	ErrInvalidTokenID = errors.New("invalid token id")
-	ErrTokenNotFound  = errors.New("token not found")
+	ErrInvalidTokenID       = errors.New("invalid token id")
+	ErrTokenNotFound        = errors.New("token not found")
+	ErrMochiProfileNotFound = errors.New("mochi profile not found")
 )
