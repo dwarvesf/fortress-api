f2f599232fff4099eb3766121dfabdca7423d501
Bean Vo <bean@d.foundation>
2024-07-19 17:48:44 +0700

    fix: add get research topic query by days

--- pkg/model/research_topic.go
+++ pkg/model/research_topic.go
@@ -1,11 +1,14 @@
 package model
 
+import "time"
+
 // DiscordResearchTopic represents discord research topic
 type DiscordResearchTopic struct {
 	Name              string
 	URL               string
 	MsgCount          int64
 	SortedActiveUsers []DiscordTopicActiveUser
+	LastActiveTime    time.Time
 }
 
 // DiscordTopicActiveUser represents active users who send most messages in topic
